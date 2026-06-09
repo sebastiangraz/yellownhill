35f3973ef4cee17f6ad825066929033f2115c20a
Sebastian Graz <graz@live.se>
2026-06-09 18:25:08 +0200

    test dep

--- vite.config.ts
+++ vite.config.ts
@@ -10,5 +10,5 @@ export default defineConfig({
   resolve: {
     tsconfigPaths: true,
   },
-  plugins: [tanstackStart(), viteReact(), netlify()],
+  plugins: [tanstackStart(), netlify(), viteReact()],
 });
