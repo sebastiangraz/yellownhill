168351e68bbd55210f78e37e7933ac31eadeccaa
sebastiangraz <graz@live.se>
2026-06-24 18:59:00 +0200

    debug

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -21,7 +21,7 @@ function RouteComponent() {
 
     setStatus("submitting");
     try {
-      const res = await fetch("/", {
+      const res = await fetch("/__forms.html", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
         body: new URLSearchParams(data as unknown as Record<string, string>),
